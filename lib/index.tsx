14407e790daeba5b6f2ce947efb77dbd0b18b38f
kishansripada <kishansripada@gmail.com>
2024-02-15 11:43:30 -0500

    switch to create root

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,21 +1,22 @@
 // import App from "../src/App";
 import { useEffect, useRef } from "react";
 // import { Index } from "../src/Widget/Index";
-import MCQ from "../src/Widget/Questions/MCQ";
-import YesOrNo from "../src/Widget/Questions/YesOrNo";
-import ReactDOM from "react-dom";
+// import MCQ from "../src/Widget/Questions/MCQ";
+// import YesOrNo from "../src/Widget/Questions/YesOrNo";
+// import ReactDOM from "react-dom";
 import "construct-style-sheets-polyfill";
 
 import { twind, cssom, observe } from "@twind/core";
 import config from "../twind.config";
 import { Index } from "@/Widget/Index";
+import { createRoot } from "react-dom/client";
 
 const HolyMetrics = (params) => {
    const ref = useRef(null);
    const sheet = cssom(new CSSStyleSheet());
    const tw = twind(config, sheet);
    useEffect(() => {
-      const shadowRoot = ref.current.shadowRoot || ref.current.attachShadow({ mode: "open" });
+      const shadowRoot = ref?.current?.shadowRoot || ref?.current?.attachShadow({ mode: "open" });
 
       shadowRoot.adoptedStyleSheets = [sheet.target];
       observe(tw, shadowRoot);
@@ -23,50 +24,57 @@ const HolyMetrics = (params) => {
       // Create a container element
       const container = document.createElement("div");
 
-      // Render your React component inside the container
-      ReactDOM.render(<Index {...params} />, container);
+      const root = createRoot(container);
+      root.render(<Index {...params} />);
 
-      // Append the container with the React component to the Shadow DOM
+      // Append container to shadowRoot
       shadowRoot.appendChild(container);
+
+      // // Render your React component inside the container
+      // ReactDOM.render(<Index {...params} />, container);
+
+      // // Append the container with the React component to the Shadow DOM
+      // shadowRoot.appendChild(container);
 
       // Clean up on component unmount
       return () => {
-         ReactDOM.unmountComponentAtNode(container);
+         // ReactDOM.unmountComponentAtNode(container);
+         root.unmount();
       };
    }, []);
 
    return <div ref={ref}></div>;
 };
 
-const MCQShadow = (params) => {
-   const ref = useRef(null);
-   const sheet = cssom(new CSSStyleSheet());
-   const tw = twind(config, sheet);
-   useEffect(() => {
-      // if (ref.current.shadowRoot) return;
-      console.log("RENDEr");
-      const shadowRoot = ref.current.shadowRoot || ref.current.attachShadow({ mode: "open" });
+// const MCQShadow = (params) => {
+//    const ref = useRef(null);
+//    const sheet = cssom(new CSSStyleSheet());
+//    const tw = twind(config, sheet);
+//    useEffect(() => {
+//       // if (ref.current.shadowRoot) return;
+//       console.log("RENDEr");
+//       const shadowRoot = ref.current.shadowRoot || ref.current.attachShadow({ mode: "open" });
 
-      shadowRoot.adoptedStyleSheets = [sheet.target];
-      observe(tw, shadowRoot);
+//       shadowRoot.adoptedStyleSheets = [sheet.target];
+//       observe(tw, shadowRoot);
 
-      // Create a container element
-      const container = document.createElement("footer");
+//       // Create a container element
+//       const container = document.createElement("footer");
 
-      // Render your React component inside the container
-      ReactDOM.render(<MCQ {...params} />, container);
+//       // Render your React component inside the container
+//       ReactDOM.render(<MCQ {...params} />, container);
 
-      // Append the container with the React component to the Shadow DOM
-      shadowRoot.appendChild(container);
+//       // Append the container with the React component to the Shadow DOM
+//       shadowRoot.appendChild(container);
 
-      // Clean up on component unmount
-      return () => {
-         ReactDOM.unmountComponentAtNode(container);
-      };
-   }, []);
+//       // Clean up on component unmount
+//       return () => {
+//          ReactDOM.unmountComponentAtNode(container);
+//       };
+//    }, []);
 
-   return <div ref={ref}></div>;
-};
+//    return <div ref={ref}></div>;
+// };
 
 export default HolyMetrics;
-export { MCQ, MCQShadow, YesOrNo };
+// export { MCQ, MCQShadow, YesOrNo };
