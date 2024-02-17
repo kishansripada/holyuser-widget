9ca0b280eb1569768aaa633ad9df4291c43c4d5f
kishansripada <kishansripada@gmail.com>
2024-02-17 12:44:48 -0500

    clean up

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,18 +1,10 @@
-// import App from "../src/App";
 import React from "react";
-// import { Index } from "../src/Widget/Index";
-// import MCQ from "../src/Widget/Questions/MCQ";
-// import YesOrNo from "../src/Widget/Questions/YesOrNo";
-// import ReactDOM from "react-dom";
 import "construct-style-sheets-polyfill";
-// // import React from "react";
 import { twind, cssom, observe } from "@twind/core";
 import config from "../twind.config";
 import { Index } from "@/Widget/Index";
 import { createRoot } from "react-dom/client";
-// import { createRoot } from "react-dom/client";
 
-// import ReactDOM from "react-dom/client";
 async function HolyWidget(params) {
    const sheet = cssom(new CSSStyleSheet());
    const tw = twind(config, sheet);
@@ -23,8 +15,6 @@ async function HolyWidget(params) {
    shadowRoot.adoptedStyleSheets = [sheet.target];
 
    observe(tw, shadowRoot);
-
-   // widget.id = "root";
    document.body.appendChild(widget);
 
    createRoot(widget.shadowRoot).render(
@@ -35,77 +25,3 @@ async function HolyWidget(params) {
 }
 
 (window as any).HolyWidget = HolyWidget;
-// (window as any).Tour = Tour;
-
-// const HolyMetrics = (params) => {
-//    const ref = useRef(null);
-//    const sheet = cssom(new CSSStyleSheet());
-//    const tw = twind(config, sheet);
-//    useEffect(() => {
-//       const shadowRoot = ref?.current?.shadowRoot || ref?.current?.attachShadow({ mode: "open" });
-
-//       shadowRoot.adoptedStyleSheets = [sheet.target];
-//       observe(tw, shadowRoot);
-
-//       // Create a container element
-//       // const container = document.createElement("div");
-
-//       // const root =
-
-//       // ReactDOM.createRoot(container);
-//       // root.render(<Index {...params} />);
-
-//       // // Append container to shadowRoot
-//       // shadowRoot.appendChild(container);
-//       // // Render your React component inside the container
-//       // ReactDOM.render(<Index {...params} />, container);
-
-//       // // Append the container with the React component to the Shadow DOM
-//       // shadowRoot.appendChild(container);
-
-//       // Clean up on component unmount
-//       return () => {
-//          // ReactDOM.unmountComponentAtNode(container);
-//          // root.unmount();
-//       };
-//    }, []);
-
-//    return (
-//       <div ref={ref}>
-//          <Index {...params} />
-//       </div>
-//    );
-// };
-
-// const MCQShadow = (params) => {
-//    const ref = useRef(null);
-//    const sheet = cssom(new CSSStyleSheet());
-//    const tw = twind(config, sheet);
-//    useEffect(() => {
-//       // if (ref.current.shadowRoot) return;
-//       console.log("RENDEr");
-//       const shadowRoot = ref.current.shadowRoot || ref.current.attachShadow({ mode: "open" });
-
-//       shadowRoot.adoptedStyleSheets = [sheet.target];
-//       observe(tw, shadowRoot);
-
-//       // Create a container element
-//       const container = document.createElement("footer");
-
-//       // Render your React component inside the container
-//       ReactDOM.render(<MCQ {...params} />, container);
-
-//       // Append the container with the React component to the Shadow DOM
-//       shadowRoot.appendChild(container);
-
-//       // Clean up on component unmount
-//       return () => {
-//          ReactDOM.unmountComponentAtNode(container);
-//       };
-//    }, []);
-
-//    return <div ref={ref}></div>;
-// };
-
-// export default HolyMetrics;
-// export { MCQ, MCQShadow, YesOrNo };
