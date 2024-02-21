176968ef9f5dd3ea19733f5a9dd9c6dc3cc13399
kishansripada <kishansripada@gmail.com>
2024-02-21 12:21:19 -0500

    remove fed

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path, { resolve } from "path";
-import federation from "@originjs/vite-plugin-federation";
 
 export default defineConfig({
-   plugins: [
-      react(),
-      // federation({
-      //    name: "remote-app",
-      //    filename: "remoteEntry.js",
-      //    // Modules to expose
-      //    exposes: {
-      //       "./Announcement": "./src/Widget/Announcements/Vertical",
-      //    },
-      //    // shared: ["react"],
-      // }),
-   ],
+   plugins: [react()],
    define: {
       "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV || "development"),
    },
