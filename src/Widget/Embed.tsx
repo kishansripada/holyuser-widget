87169c3e1ac72a4124c07ef98179501e10d23bbc
kishansripada <kishansripada@gmail.com>
2024-02-21 12:20:32 -0500

    test importing React

--- src/Widget/Embed.tsx
+++ src/Widget/Embed.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { createClient } from "@supabase/supabase-js";
 import Poll from "./ShouldShow/Poll";
 
