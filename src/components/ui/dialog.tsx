e657c77993c9ba525272d88fca13090a95c6aac7
kishansripada <kishansripada@gmail.com>
2024-03-18 11:48:12 -0400

    small change

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -3,8 +3,6 @@ import * as React from "react";
 
 import { cn } from "@/utils";
 
-// import { X } from "lucide-react";
-
 const Dialog = DialogPrimitive.Root;
 
 const DialogTrigger = DialogPrimitive.Trigger;
@@ -34,14 +32,14 @@ const DialogContent = React.forwardRef<
 >(({ className, children, ...props }, ref) => (
    // <DialogPortal>
    <>
-      {" "}
       <DialogOverlay />
       <DialogPrimitive.Content
          ref={ref}
          className={cn(
-            "data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] fixed left-[50%] top-[50%] z-50 grid w-full max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4 border border-neutral-200 bg-white shadow-lg duration-200 sm:rounded-lg dark:border-neutral-800 dark:bg-neutral-950",
+            "data-[state=open]:animate-in  data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] min-w-lg fixed left-[50%] top-[50%] z-50 grid w-min translate-x-[-50%] translate-y-[-50%] gap-4 border border-neutral-200 bg-white shadow-lg duration-200 sm:rounded-lg dark:border-neutral-800 dark:bg-neutral-950",
             className
          )}
+         // data-state={props.open}
          {...props}
       >
          {children}
