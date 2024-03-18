7dfc74dfc7a75998750cccb970732cd75264560e
kishansripada <kishansripada@gmail.com>
2024-03-18 11:44:54 -0400

    allow youtube embeds

--- src/Widget/Messages/Modal/default-modal.tsx
+++ src/Widget/Messages/Modal/default-modal.tsx
@@ -4,7 +4,7 @@ import { poll } from "@/typesandconst";
 
 export default function DefaultModal({ poll, sendResponse }: { poll: poll; sendResponse: Function }) {
    return (
-      <div className="flex h-full w-full flex-col gap-3 p-6  ">
+      <div className=" flex h-full w-full flex-col gap-3  p-6 ">
          <div className="flex flex-row items-start justify-between  ">
             <p className=" text-xl font-bold tracking-tight text-neutral-900 dark:text-neutral-100">{poll.poll_data.title}</p>
             <button
@@ -18,7 +18,22 @@ export default function DefaultModal({ poll, sendResponse }: { poll: poll; sendR
 
          <p className=" text-sm text-neutral-600 dark:text-neutral-400">{poll.poll_data.subtitle}</p>
 
-         {poll.poll_data.image_url && (
+         {poll.poll_data.image_url.includes("www.youtube.com") ? (
+            <iframe
+               style={{
+                  height: 350,
+                  width: 600,
+               }}
+               // width="560"
+               // height="315"
+               className="h-full w-full rounded-md rounded-xl "
+               src={poll.poll_data.image_url.replace("watch?v=", "embed/")}
+               title="YouTube video player"
+               frameborder="0"
+               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+               allowFullScreen
+            ></iframe>
+         ) : poll.poll_data.image_url ? (
             <div className="h-full w-full overflow-hidden">
                <img
                   className="h-full w-full rounded-md rounded-xl border border-neutral-300 object-cover dark:border-neutral-700"
@@ -26,7 +41,7 @@ export default function DefaultModal({ poll, sendResponse }: { poll: poll; sendR
                   alt=""
                />
             </div>
-         )}
+         ) : null}
 
          <div className="mt-1 flex flex-row items-center  justify-between border-neutral-300 font-medium dark:border-neutral-700 ">
             <div></div>
