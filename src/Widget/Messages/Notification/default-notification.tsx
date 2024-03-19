858b204d32ad2375885f3f34ccb22af5cd2af21a
kishansripada <kishansripada@gmail.com>
2024-03-19 11:21:17 -0400

    noitification subtitle

--- src/Widget/Messages/Notification/default-notification.tsx
+++ src/Widget/Messages/Notification/default-notification.tsx
@@ -5,7 +5,7 @@ export default function DefaultNotification({ poll, sendResponse }: { poll: any;
       <div className="flex h-full w-full flex-row justify-between gap-1 p-4 ">
          <div className="flex flex-col items-start gap-1  ">
             <p className="text-sm font-bold tracking-tight text-neutral-900 dark:text-neutral-100">{poll.poll_data.title}</p>
-            <p className="text-xs text-neutral-600 dark:text-neutral-400">{poll.poll_data.subtitle}</p>
+            {poll.poll_data.subtitle && <p className="text-xs text-neutral-600 dark:text-neutral-400">{poll.poll_data.subtitle}</p>}
          </div>
 
          {/* {poll.poll_data.image_url && (
