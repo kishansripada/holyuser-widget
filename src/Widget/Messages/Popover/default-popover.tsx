3ce51850bf10dc3cfacebd6dd919916940979074
kishansripada <kishansripada@gmail.com>
2024-03-26 12:55:13 -0400

    markdown popover

--- src/Widget/Messages/Popover/default-popover.tsx
+++ src/Widget/Messages/Popover/default-popover.tsx
@@ -1,31 +1,62 @@
 import { Button } from "@/components/ui/button";
 import { XIcon } from "@/assets/icons";
 
-export default function DefaultPopover({ poll, sendResponse }: { poll: any; sendResponse: Function }) {
+export default function DefaultPopover({ poll, sendResponse, markdown }: { poll: any; sendResponse: Function }) {
+   const blocks = markdown.split("\n");
+   // console.log(blocks);
+
+   const h1 = (text: string) => <h1 className="text-sm font-semibold tracking-tight text-neutral-900 dark:text-neutral-100">{text}</h1>;
+   const ComButton = () => (
+      <Button
+         className="mt-2"
+         // variant={"outline"}
+         onClick={() => {
+            sendResponse({ choice: "next_step" });
+         }}
+         size={"sm"}
+      >
+         Got it
+      </Button>
+   );
+
    return (
-      <div className="flex flex-col gap-3 p-4 ">
-         <div className="flex flex-col items-start gap-1 ">
+      <div className="flex flex-col p-4 ">
+         {blocks.map((block, index) => {
+            if (block.startsWith("#")) {
+               return h1(block.replace("#", "").trim());
+            } else if (block.startsWith("<") && block.endsWith("/>")) {
+               const tag = block.replace("<", "").replace(">", "").replace("/", "");
+               console.log(tag);
+               if (tag.trim() === "Button") return <ComButton key={index}></ComButton>;
+
+               return (
+                  <p key={index} className="mt-1 text-xs text-neutral-600 dark:text-neutral-400">
+                     {block}
+                  </p>
+               );
+            } else {
+               return (
+                  <p key={index} className="mt-1 text-xs text-neutral-600 dark:text-neutral-400">
+                     {block}
+                  </p>
+               );
+            }
+         })}
+         <button
+            className="absolute right-[16px] top-[16px]"
+            onClick={() => {
+               sendResponse({ choice: "end_deployment" });
+            }}
+         >
+            <XIcon></XIcon>
+         </button>
+
+         {/* <div className="flex flex-col items-start gap-1 ">
             <div className="flex w-full flex-row items-start justify-between">
-               <p className="text-sm font-semibold tracking-tight text-neutral-900 dark:text-neutral-100">{poll.poll_data.title}</p>
-               <button
-                  onClick={() => {
-                     sendResponse({ choice: "end_deployment" });
-                  }}
-               >
-                  <XIcon></XIcon>
-               </button>
+               <h1 className="text-sm font-semibold tracking-tight text-neutral-900 dark:text-neutral-100">{poll.poll_data.title}</h1>
             </div>
             <p className="text-xs text-neutral-600 dark:text-neutral-400">{poll.poll_data.subtitle}</p>
-         </div>
-         <Button
-            // variant={"outline"}
-            onClick={() => {
-               sendResponse({ choice: "next_step" });
-            }}
-            size={"sm"}
-         >
-            Got it
-         </Button>
+         </div> */}
       </div>
    );
 }
