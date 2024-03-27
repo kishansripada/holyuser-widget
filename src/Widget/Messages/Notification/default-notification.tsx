92dbac745e7c606e4584457c279c4caceb5243db
kishansripada <kishansripada@gmail.com>
2024-03-27 12:58:49 -0400

    notification takes markdown

--- src/Widget/Messages/Notification/default-notification.tsx
+++ src/Widget/Messages/Notification/default-notification.tsx
@@ -1,34 +1,94 @@
 import { XIcon } from "@/assets/icons";
+import { Button } from "@/components/ui/button";
 
 export default function DefaultNotification({ poll, sendResponse }: { poll: any; sendResponse: Function }) {
-   return (
-      <div className="flex h-full w-full flex-row justify-between gap-1 p-4 ">
-         <div className="flex flex-col items-start gap-1  ">
-            <p className="text-xs font-semibold  text-neutral-800 dark:text-neutral-100">{poll.poll_data.title}</p>
-            {poll.poll_data.subtitle && <p className="text-xs text-neutral-500 dark:text-neutral-400">{poll.poll_data.subtitle}</p>}
-         </div>
+   if (!poll.markdown) return <></>;
+   const blocks = poll.markdown.split("\n");
+   // console.log(blocks);
 
-         {/* {poll.poll_data.image_url && (
-            <div className="h-full  overflow-hidden">
-               <img
-                  style={{
-                     width: 85,
-                  }}
-                  className="h-full rounded-md rounded-xl border border-neutral-300 object-cover dark:border-neutral-700"
-                  src={poll.poll_data.image_url}
-                  alt=""
-               />
+   const h1 = (text: string) => <h1 className="text-xs font-semibold  text-neutral-800 dark:text-neutral-100">{text}</h1>;
+   const ComButton = () => (
+      <Button
+         className="mt-2 w-full"
+         // variant={"outline"}
+         onClick={() => {
+            sendResponse({ choice: "next_step" });
+         }}
+         size={"sm"}
+      >
+         Got it
+      </Button>
+   );
+   return (
+      <div className="flex flex-col items-start  p-4  ">
+         {blocks.map((block, index) => {
+            if (block.startsWith("#")) {
+               return (
+                  <div
+                     style={{
+                        marginRight: index === 0 ? "20px" : "0",
+                     }}
+                  >
+                     {h1(block.replace("#", ""))}
+                  </div>
+               );
+            } else if (block.startsWith("<") && block.endsWith("/>")) {
+               const tag = block.replace("<", "").replace(">", "").replace("/", "");
+
+               if (tag.trim() === "Button")
+                  return (
+                     <div
+                        className="w-full"
+                        style={{
+                           marginRight: index === 0 ? "20px" : "0",
+                        }}
+                     >
+                        <ComButton key={index}></ComButton>
+                     </div>
+                  );
+
+               return (
+                  <p
+                     style={{
+                        marginRight: index === 0 ? "20px" : "0",
+                     }}
+                     key={index}
+                     className="mt-1 text-xs text-neutral-500 dark:text-neutral-400"
+                  >
+                     {block}
+                  </p>
+               );
+            } else {
+               return (
+                  <p
+                     style={{
+                        marginRight: index === 0 ? "20px" : "0",
+                     }}
+                     key={index}
+                     className="mt-1 text-xs text-neutral-500 dark:text-neutral-400"
+                  >
+                     {block}
+                  </p>
+               );
+            }
+         })}
+         <button
+            className="absolute right-[16px] top-[16px]"
+            onClick={() => {
+               sendResponse({ choice: "end_deployment" });
+            }}
+         >
+            <XIcon></XIcon>
+         </button>
+
+         {/* <div className="flex flex-col items-start gap-1 ">
+            <div className="flex w-full flex-row items-start justify-between">
+               <h1 className="text-sm font-semibold tracking-tight text-neutral-900 dark:text-neutral-100">{poll.poll_data.title}</h1>
             </div>
-         )} */}
-         <div className="flex h-full flex-col ">
-            <button
-               onClick={() => {
-                  sendResponse({ option_id: "close" });
-               }}
-            >
-               <XIcon></XIcon>
-            </button>
-         </div>
+            <p className="text-xs text-neutral-600 dark:text-neutral-400">{poll.poll_data.subtitle}</p>
+         </div> */}
+         {/* <p className="text-xs font-semibold  text-neutral-800 dark:text-neutral-100">{poll.poll_data.title}</p>
+            {poll.poll_data.subtitle && <p className="text-xs text-neutral-500 dark:text-neutral-400">{poll.poll_data.subtitle}</p>} */}
       </div>
    );
 }
