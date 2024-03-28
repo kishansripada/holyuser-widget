72a37b2504cf4dbbb2d3b30bdd383bb376ac31c9
kishansripada <kishansripada@gmail.com>
2024-03-27 23:40:22 -0400

    markdown modal

--- src/Widget/Messages/Modal/default-modal.tsx
+++ src/Widget/Messages/Modal/default-modal.tsx
@@ -3,18 +3,79 @@ import { XIcon } from "@/assets/icons";
 import { poll } from "@/typesandconst";
 
 export default function DefaultModal({ poll, sendResponse }: { poll: poll; sendResponse: Function }) {
+   poll.markdown = `#Feedback is always appreciated
+   We promise we'll reply in 24 hours, sometimes much faster :)
+<Button />`;
+   if (!poll.markdown) return <></>;
+   const blocks = poll.markdown.split("\n");
+   const h1 = (text: string) => <p className=" text-xl font-bold tracking-tight text-neutral-900 dark:text-neutral-100">{text}</p>;
+   const hushed = (text: string) => <p className=" text-sm text-neutral-600 dark:text-neutral-400">{text}</p>;
+   const ComButton = () => (
+      <Button
+         className="mt-2 w-full"
+         // variant={"outline"}
+         onClick={() => {
+            sendResponse({ choice: "next_step" });
+         }}
+         size={"sm"}
+      >
+         Got it
+      </Button>
+   );
    return (
       <div className=" flex h-full w-full flex-col gap-3  p-6 ">
-         <div className="flex flex-row items-start justify-between  ">
-            <p className=" text-xl font-bold tracking-tight text-neutral-900 dark:text-neutral-100">{poll.poll_data.title}</p>
-            <button
-               onClick={() => {
-                  sendResponse({ choice: "end_deployment" });
-               }}
-            >
-               <XIcon></XIcon>
-            </button>
-         </div>
+         <button
+            className="absolute right-[24px] top-[24px]"
+            onClick={() => {
+               sendResponse({ choice: "end_deployment" });
+            }}
+         >
+            <XIcon></XIcon>
+         </button>
+
+         {blocks.map((block, index) => {
+            if (block.startsWith("#")) {
+               return (
+                  <div
+                     style={{
+                        marginRight: index === 0 ? "20px" : "0",
+                     }}
+                  >
+                     {h1(block.replace("#", ""))}
+                  </div>
+               );
+            } else if (block.startsWith("<") && block.endsWith("/>")) {
+               const tag = block.replace("<", "").replace(">", "").replace("/", "");
+
+               if (tag.trim() === "Button")
+                  return (
+                     <div
+                        className="w-full"
+                        style={{
+                           marginRight: index === 0 ? "20px" : "0",
+                        }}
+                     >
+                        <ComButton key={index}></ComButton>
+                     </div>
+                  );
+
+               return (
+                  <p
+                     style={{
+                        marginRight: index === 0 ? "20px" : "0",
+                     }}
+                     key={index}
+                     className="mt-1 text-xs text-neutral-500 dark:text-neutral-400"
+                  >
+                     {block}
+                  </p>
+               );
+            } else {
+               return hushed(block);
+            }
+         })}
+
+         {/* <p className=" text-xl font-bold tracking-tight text-neutral-900 dark:text-neutral-100">{poll.poll_data.title}</p>
 
          <p className=" text-sm text-neutral-600 dark:text-neutral-400">{poll.poll_data.subtitle}</p>
 
@@ -36,7 +97,7 @@ export default function DefaultModal({ poll, sendResponse }: { poll: poll; sendR
          ) : poll.poll_data.image_url ? (
             <div className="h-full w-full overflow-hidden">
                <img
-                  className="h-full w-full rounded-md rounded-xl border border-neutral-300 object-cover dark:border-neutral-700"
+                  className="h-full w-full  rounded-xl border border-neutral-300 object-cover dark:border-neutral-700"
                   src={poll.poll_data.image_url}
                   alt=""
                />
@@ -53,7 +114,7 @@ export default function DefaultModal({ poll, sendResponse }: { poll: poll; sendR
             >
                Okay, let's go!
             </Button>
-         </div>
+         </div> */}
       </div>
    );
 }
